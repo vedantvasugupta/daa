93222c78604d01fe586595ade0eb054c70892933
vedantvasugupta <76390819+vedantvasugupta@users.noreply.github.com>
2022-05-23 23:28:18 +0530

    Update chain_matrix_multiplIcation.c

--- 4th Semester/Lab 9/chain_matrix_multiplIcation.c
+++ 4th Semester/Lab 9/chain_matrix_multiplIcation.c
@@ -1,69 +1,83 @@
-#include <stdio.h>
-int *  fill_cost_matrix(int *cost_matrix, int *location_matrix, int *dimensions, int n);
-
+//matrix chain multiplication in c by aryan srivastava
+#include<stdio.h>
 int main()
 {
-    int n;
-    printf("Input the number of matrixes: ");
+    int n;      //no of matrix
+    printf("Enter number of matrix - > ");
     scanf("%d", &n);
-    printf("Input the dimensions of the matrices: ");
-    int *dimensions = malloc(sizeof(int) * (n + 1));
-    for (int i = 0; i < n + 1; i++)
+
+    int d[2*n]; //dimensions of n matricies
+    printf("Enter the dimesions of matricies -> ");
+    for(int i = 0; i<2*n; i++)
+        scanf("%d", &d[i]);
+    
+    int dim[n+1], a = 0;
+
+    for(int i=0; i<n+1; i++)
     {
-        scanf("%d", (dimensions + i));
+        if(i==n)
+            dim[i] = d[2*n-1];
+        else
+            dim[i] = d[a];
+        a +=2;
     }
-    // print dimensions
-    for (int i = 0; i < n + 1; i++)
-    {
-        printf("%d ", *(dimensions + i));
+
+    // for(int i =0; i<n+1; i++)
+    //     printf("dim[%d] = %d\n", i, dim[i]);
+
+
+    int mc[n][n], m_k[n][n], x, y, min, min_k;
+
+    for(int i =0 ; i < n; i++)  //assigning diagonal values to 0
+        mc[i][i] = 0;
+    
+
+    int j, t=0;
+    while(t!=n-1)
+    { 
+        for (int i = 0; i<n-1; i++) 
+        {
+            j = i+t+1; 
+            x = 10000000;
+                for(int k = i; k<j; k++) 
+                {
+                    if(k==i)
+                    {
+                        min = mc[i][k] + mc[k+1][j] + dim[i]*dim[j+1]*dim[k+1];
+                        min_k = k;
+                    }  
+                    else 
+                    {
+                        x = mc[i][k] + mc[k+1][j] + dim[i]*dim[j+1]*dim[k+1]; 
+                    }
+
+                    if(min > x) 
+                    {
+                        min = x;
+                        min_k = k;
+                    }
+                }
+                mc[i][j] = min;
+                m_k[i][j]= min_k;
+        }
+        t++;
     }
-    printf("Successfully saved matrices' dimensions \n");
-    int *cost_matrix = malloc(sizeof(int *) * n * n);
-    for (int i = 0; i < n; i++)
-    {
-        *(cost_matrix + i * n + i) = 0;
-    }
-    printf("Successfully allocated cost matrix \n");
-    int *location_matrix = malloc(sizeof(int) * n * n);
-    printf("Trying to fill the cost matrice\n ");
-    cost_matrix = fill_cost_matrix(cost_matrix, location_matrix, dimensions, n);
+
+    // for(int i =0; i<n; i++)
+    // {
+    //     for(int j=0; j<n; j++)
+    //     {
+    //         printf("%d\t\t", m_k[i][j]);
+    //     }
+    //     printf("\n\n");
+    // }
+
+    int minimum = mc[0][n-1];
+    printf("\nMinimum = %d\n", minimum);
+    return 0;    
 }
 
-int *  fill_cost_matrix(int *cost_matrix, int *location_matrix, int *dimensions, int n)
-{
-    for (int k = 1; k < n; k++)
-    {
-        for (int i = 0, j = k; i < n, j < n; i++, j++)
-        {
-            // printf("cost_matrix[%d,%d]: ", i, j, *(cost_matrix + i * n + j));
-            *(cost_matrix + i * n + j) = formula(dimensions, cost_matrix, i, j, n);
-            // printf("%d ", i, j, *(cost_matrix + i * n + j));
-        }
-        printf("\n");
-    }
-    return cost_matrix; 
-}
-
-int formula(int *dimensions, int **cost_matrix, int i, int j, int n)
-{
-    if (j == 0)
-    {
-        return 0;
-    }
-    int min_cost = 999999;
-    for (int k = i; k < j; k++)
-    {
-        int temp_cost = 0;
-        printf("Adding %d", *(dimensions + i) * (*(dimensions + k + 1)) * (*(dimensions + j + 1)));
-        temp_cost = temp_cost +  *(cost_matrix + i * n + k);
-        temp_cost = temp_cost + *(cost_matrix + (k + 1) * n + j);
-        temp_cost = temp_cost + *(dimensions + i) * (*(dimensions + k + 1)) * (*(dimensions + j + 1));
-        printf("So in this itteration the temp cost is : %d\n", temp_cost);
-        if (temp_cost < min_cost)
-        {
-            printf("Chaning min cost to %d\n", temp_cost);
-            min_cost = temp_cost;
-        }
-    }
-    return min_cost;
-}
+// 3
+// 3 5 5 7 7 5
+// 4
+// 2 4 4 6 6 8 8 10
